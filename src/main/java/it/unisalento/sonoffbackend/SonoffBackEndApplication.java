aa62ff761549662d171a71051771e00f76757462
Simone Russo <34507615+simonerusso97@users.noreply.github.com>
2022-03-01 15:52:33 +0100

    clean up code

--- src/main/java/it/unisalento/sonoffbackend/SonoffBackEndApplication.java
+++ src/main/java/it/unisalento/sonoffbackend/SonoffBackEndApplication.java
@@ -1,16 +1,12 @@
 package it.unisalento.sonoffbackend;
 
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SonoffBackEndApplication {
-	private static final Logger LOG = LogManager.getLogger(SonoffBackEndApplication.class);
-
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		SpringApplication.run(SonoffBackEndApplication.class, args);
 	}
 
--- src/main/java/it/unisalento/sonoffbackend/exception/InvalidTokenEx.java
+++ src/main/java/it/unisalento/sonoffbackend/exception/InvalidTokenEx.java
@@ -1,6 +1,10 @@
 package it.unisalento.sonoffbackend.exception;
 
 public class InvalidTokenEx extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	String message;
 	public InvalidTokenEx(String message) {
 		super();
